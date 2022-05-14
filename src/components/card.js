4de4570e836ac751bf3a0aecfce2b2d5efb2fe6b
Geethika Singareddy <geethika1129@gmail.com>
2022-05-14 15:26:37 +0530

    fix

--- src/components/card.js
+++ src/components/card.js
@@ -22,15 +22,15 @@ useEffect(() => {
                   <div class="shadow p-3 mb-5 bg-white rounded " id='cint'>
              
                                  <div className="center">
-                                <img class="card-img-top imgg rounded mx-auto d-block" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqWIPKg9kRQhn9r3qgpcRSACAXvg-dbTOWQiDN6b5ahLRZ-AU_ioL_uXv5Un0kNGPNhE&usqp=CAU' alt='img'/>
+                                <img class="card-img-top imgg rounded mx-auto d-block" src={props.avatar_url} alt='img'/>
                                 </div><div class="card-body ">
-                                  <h5 class="card-title">{props.login}</h5>
-                                  <p class="card-text">{props.url}</p>
+                                  <h5 class="card-title"><center>{props.login}</center></h5>
+                                  <p class="card-text"><i><center>{props.url}</center></i></p>
                                 </div>
                                 <ul class="list-group list-group-flush">
-                                  <li class="list-group-item">{props.followers_url}</li>
-                                  <li class="list-group-item">{props.following_url}</li>
-                                  <li class="list-group-item">{props.starred_url}</li>
+                                  <li class="list-group-item"><b>Followers : </b>40</li>
+                                  <li class="list-group-item"><b>Following : </b>23</li>
+                                  <li class="list-group-item"><b>Repositories : </b>19</li>
                                 </ul>
                               </div>
                  </div>
